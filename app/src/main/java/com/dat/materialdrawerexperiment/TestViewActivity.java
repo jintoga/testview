22b8c32a711e080607f6f5e5150754724a60f5b4
Dat <jintoga123@yahoo.com>
2016-12-21 00:36:27 +0700

    fixed

--- app/src/main/java/com/dat/materialdrawerexperiment/TestViewActivity.java
+++ app/src/main/java/com/dat/materialdrawerexperiment/TestViewActivity.java
@@ -1,13 +1,21 @@
 package com.dat.materialdrawerexperiment;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class TestViewActivity extends AppCompatActivity {
+
+    @Bind(R.id.cheque)
+    protected WebView cheque;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_view);
+        ButterKnife.bind(this);
+        cheque.loadUrl("file:///android_asset/template.html");
     }
 }
