2074087263c1c0b7fbdbe337f1e9a6f21fbfe169
Dat <jintoga123@yahoo.com>
2016-12-20 23:59:05 +0700

    added

---
+++ app/src/main/java/com/dat/materialdrawerexperiment/TestViewActivity.java
@@ -0,0 +1,13 @@
+package com.dat.materialdrawerexperiment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_view);
+    }
+}
